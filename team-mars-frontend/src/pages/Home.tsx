c41c9712dc7770aec500fa75ff7f5fe269ef8e6a
ejminguez <ebminguez@up.edu.ph>
2025-11-18 14:27:13 +0800

    feat: add placeholder navbar and outlet element

--- team-mars-frontend/src/pages/Home.tsx
+++ team-mars-frontend/src/pages/Home.tsx
@@ -1,7 +1,5 @@
-const Home = ( ) => {
-     return (
-          <h1>Landing Page</h1>
-     )
-}
+const Home = () => {
+  return <h1>Landing Page</h1>;
+};
 
 export default Home;
---
+++ team-mars-frontend/src/router/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App.tsx';
+import Home from '@/pages/Home.tsx';
+import Contact from '@/pages/Contact';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'contact',
+        element: <Contact/>
+      }
+    ],
+  },
+]);
