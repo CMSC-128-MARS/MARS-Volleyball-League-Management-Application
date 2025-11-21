75ed52c7e8819278ddd0fcdd4a9cb3ec75bc8dde
Lanoia <bctaclindo2@up.edu.ph>
2025-11-21 21:24:08 +0800

    feat: add footer

--- team-mars-frontend/src/App.tsx
+++ team-mars-frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/navigation/LoginNavbar';
 
+
 function App() {
   return (
+
     <div className="flex flex-col h-screen">
       <Navbar />
       <div className="flex-1 overflow-auto">
