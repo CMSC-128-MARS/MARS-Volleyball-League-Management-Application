9ce2a22a854f752860f01166f158dc99aad17f6c
Lanoia <bctaclindo2@up.edu.ph>
2025-11-21 18:49:57 +0800

    style: adjust hero button padding

--- team-mars-frontend/src/components/ui/ButtonVariants.tsx
+++ team-mars-frontend/src/components/ui/ButtonVariants.tsx
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[2px] paragraph-n-medium transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive dark:aria-invalid:ring-destructive aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[2px] pg1-bold  transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive dark:aria-invalid:ring-destructive aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -24,9 +24,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-[32px] rounded-[2px] gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-[48px] rounded-[2px] px-6 has-[>svg]:px-4",
+        default: "h-auto px-[16px] py-[8px] gap-[8px] has-[>svg]:px-3",
+        sm: "h-auto rounded-[2px] px-[12px] py-[6px] gap-[4px] has-[>svg]:px-2.5",
+        lg: "h-auto rounded-[2px] px-[36px] py-[16px] gap-[8px] has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
