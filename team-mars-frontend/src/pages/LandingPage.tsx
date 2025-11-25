e62f5091f76c929a09cdf4d48fc894b214ee5371
Francis Reid <143309990+Celeru00@users.noreply.github.com>
2025-11-26 01:15:57 +0800

    Revert "style: adjust hero button padding"
    This reverts commit c0926315b6fd69916d7888c843b396e7261b130c.

--- team-mars-frontend/src/pages/LandingPage.tsx
+++ team-mars-frontend/src/pages/LandingPage.tsx
@@ -34,7 +34,7 @@ const LandingPage = () => {
           <h2 className="heading-hero-responsive uppercase text-center">Volleyball League Management System</h2>
         </div>
         {/* Buttons */}
-        <div className="flex items-center">
+        <div className="flex gap-4 items-center">
           <Button variant="secondary" size="lg" className="text-primary uppercase ">
           Sign In
           </Button>
