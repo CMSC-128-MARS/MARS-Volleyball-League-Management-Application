c3df826f95ae3a30ab3a8d0a3a6ab803e7f4415d
Francis Reid <143309990+Celeru00@users.noreply.github.com>
2025-11-26 01:15:57 +0800

    Revert "refactor: migrate inline Tailwind to responsive CSS utilities in hero section"
    This reverts commit a2efcd26b6c7c7088578fe5fc7a5a42c01136e0e.

--- team-mars-frontend/src/pages/LandingPage.tsx
+++ team-mars-frontend/src/pages/LandingPage.tsx
@@ -23,19 +23,20 @@ const LandingPage = () => {
         alt="Hero Element 2"  
         className="hero-bg-element-base hero-bg-element-bottom-left absolute"
       />
+      
       {/* Main */}
-      <div className="hero-padding-responsive">
+      <div className="relative z-10 flex flex-col items-center justify-center margin-lr-responsive gap-[32px]">
         {/* Logo and subheading */}
-        <div className="flex flex-col items-center gap-[8px]">
+        <div className="mt-[148px] flex flex-col items-center gap-[8px]">
           <img 
           src="./src/assets/MARS-logo.png" 
           alt="MARS Logo"
-          className="w-auto h-auto object-contain"/>
-          <h2 className="heading-hero-responsive uppercase text-center">Volleyball League Management System</h2>
+          className="logo-responsive object-contain"/>
+          <h2 className="heading-responsive uppercase">Volleyball League Management System</h2>
         </div>
         {/* Buttons */}
-        <div className="flex gap-4 items-center">
-          <Button variant="secondary" size="lg" className="text-primary uppercase ">
+        <div className="flex gap-4 items-center mb-[148px]">
+          <Button variant="secondary" size="lg" className="text-primary uppercase">
           Sign In
           </Button>
         </div>
@@ -54,31 +55,27 @@ const LandingPage = () => {
         {/* Content */}
         <div className="features-content-responsive">
           <div className="features-card-responsive">
-            <Users2 className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Efficient Player Management</h3>
+            <Users2 className="features-icon-responsive text-secondary"/>
+            <h3 className="text-secondary py-[8px]">Efficient Player Management</h3>
             <div className="w-full h-[2px] bg-secondary"/>
             <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">
               Go beyond basic details with our optimized Player Management feature. This system allows you to effortlessly record, update, and track all necessary player information.
             </p>
           </div>
           <div className="features-card-responsive">
-            <Volleyball className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Simplified Team Organization</h3>
+            <Volleyball className="features-icon-responsive text-secondary"/>
+            <h3 className="text-secondary py-[8px]">Simplified Team Organization</h3>
             <div className="w-full h-[2px] bg-secondary rounded" />
             <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">A comprehensive system that significantly simplifies the organization of all teams by providing a straightforward interface for assigning players, managing team details, and handling administrative updates.</p>
           </div>
           <div className="features-card-responsive">
-            <Trophy className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Effortless League Creation</h3>
+            <Trophy className="features-icon-responsive text-secondary"/>
+            <h3 className="text-secondary py-[8px]">Effortless League Creation</h3>
             <div className="w-full h-[2px] bg-secondary rounded" />
             <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">Experience a seamless process for creating and managing leagues with our user-friendly tools designed to simplify every step from setup to completion.</p>
           </div>
         </div>
       </div>
-    </div>
-    {/* Footer */}
-    <div>
-
     </div>
   </>
   );
