27825d6f01444960ade8aebc8e331f30093d6e96
Francis Reid <clruru@Franciss-MacBook-Air.local>
2025-11-26 14:53:33 +0800

    feat: implemented oauth with amazon cognito

--- team-mars-frontend/src/main.tsx
+++ team-mars-frontend/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import { cognitoConfig } from './config/cognito';
 import './index.css';
 import { router } from './router/router';
+
+Amplify.configure(cognitoConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
