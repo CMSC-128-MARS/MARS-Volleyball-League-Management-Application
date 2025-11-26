d1ca1657f8e5991b3f35708fb3bc906b34541f4f
Francis Reid <143309990+Celeru00@users.noreply.github.com>
2025-11-26 21:39:57 +0800

    Revert "chore: moved and refactored img path"
    This reverts commit 56b9844675e6b3f24a15aaa1469382cff7e410d9.

--- team-mars-frontend/src/pages/LandingPage.tsx
+++ team-mars-frontend/src/pages/LandingPage.tsx
@@ -11,19 +11,19 @@ const LandingPage = () => {
       <div
         className="bg-background-alt text-primary-foreground relative overflow-hidden"
         style={{
-          backgroundImage: `url('/Dust.png')`,
+          backgroundImage: `url('./src/assets/Dust.png')`,
           backgroundSize: 'auto',
           backgroundRepeat: 'repeat',
         }}
       >
         {/* Bg Elements*/}
         <img
-          src="/assets/hero-element1.png"
+          src="./src/assets/hero-element1.png"
           alt="Hero Element 1"
           className="hero-bg-element-base hero-bg-element-bottom-right absolute"
         />
         <img
-          src="/assets/hero-element2.png"
+          src="./src/assets/hero-element2.png"
           alt="Hero Element 2"
           className="hero-bg-element-base hero-bg-element-bottom-left absolute"
         />
@@ -32,7 +32,7 @@ const LandingPage = () => {
           {/* Logo and subheading */}
           <div className="flex flex-col items-center gap-[8px]">
             <img
-              src="/assets/MARS-logo.png"
+              src="./src/assets/MARS-logo.png"
               alt="MARS Logo"
               className="w-auto h-auto object-contain"
             />
@@ -52,7 +52,7 @@ const LandingPage = () => {
       <div
         className="bg-background-alt-2 relative"
         style={{
-          backgroundImage: `url('/Grunge.png')`,
+          backgroundImage: `url('./src/assets/Grunge.png')`,
           backgroundSize: 'auto',
           backgroundRepeat: 'repeat',
         }}
