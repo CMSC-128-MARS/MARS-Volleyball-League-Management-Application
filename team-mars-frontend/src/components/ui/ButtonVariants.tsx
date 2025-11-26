a565987c13f255568eee3f5ec918413940073169
ejminguez <ebminguez@up.edu.ph>
2025-11-26 21:03:18 +0800

    fix: resolve conflicts

--- team-mars-frontend/src/components/ui/ButtonVariants.tsx
+++ team-mars-frontend/src/components/ui/ButtonVariants.tsx
@@ -1,42 +1,42 @@
-import { cva } from "class-variance-authority"
+import { cva } from 'class-variance-authority';
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[2px] pg1-bold  transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive dark:aria-invalid:ring-destructive aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        /*Primary */ 
-        default: "bg-primary primary-foreground hover:bg-primary-alt hover:text-primary-alt-foreground",
+        /*Primary */
+        default:
+          'bg-primary primary-foreground hover:bg-primary-alt hover:text-primary-alt-foreground',
         destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive dark:focus-visible:ring-destructive dark:bg-destructive",
+          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive dark:focus-visible:ring-destructive dark:bg-destructive',
         outline:
-          "border shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        "outline-primary":
-          "border border-primary text-primary bg-transparent hover:bg-primary hover:text-primary-foreground shadow-xs",
-        "outline-destructive":
-          "border border-destructive text-destructive bg-transparent hover:bg-destructive hover:text-white shadow-xs",
-        "outline-secondary":
-          "border border-secondary text-secondary-foreground bg-transparent hover:bg-secondary hover:text-secondary-foreground shadow-xs",
+          'border shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+        'outline-primary':
+          'border border-primary text-primary bg-transparent hover:bg-primary hover:text-primary-foreground shadow-xs',
+        'outline-destructive':
+          'border border-destructive text-destructive bg-transparent hover:bg-destructive hover:text-white shadow-xs',
+        'outline-secondary':
+          'border border-secondary text-secondary-foreground bg-transparent hover:bg-secondary hover:text-secondary-foreground shadow-xs',
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary-alt hover:text-secondary-alt-foreground",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          'bg-secondary text-secondary-foreground hover:bg-secondary-alt hover:text-secondary-alt-foreground',
+        ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: "h-auto px-[16px] py-[12px] gap-[8px] has-[>svg]:px-3",
-        sm: "h-auto rounded-[2px] px-[12px] py-[8px] gap-[4px] has-[>svg]:px-2.5",
-        lg: "h-auto rounded-[2px] px-[32px] py-[16px] gap-[8px] has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        default: 'h-auto px-[16px] py-[12px] gap-[8px] has-[>svg]:px-3',
+        sm: 'h-auto rounded-[2px] px-[12px] py-[8px] gap-[4px] has-[>svg]:px-2.5',
+        lg: 'h-auto rounded-[2px] px-[32px] py-[16px] gap-[8px] has-[>svg]:px-4',
+        icon: 'size-9',
+        'icon-sm': 'size-8',
+        'icon-lg': 'size-10',
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default',
     },
-  }
-)
+  },
+);
 
-export { buttonVariants }
+export { buttonVariants };
--- team-mars-frontend/src/pages/LandingPage.tsx
+++ team-mars-frontend/src/pages/LandingPage.tsx
@@ -1,84 +1,104 @@
-import { Button } from "@/components/ui/Buttons"
-import { Users2 } from "lucide-react"
-import { Volleyball } from "lucide-react";
-import { Trophy } from "lucide-react";
+import { Button } from '@/components/ui/Buttons';
+import { Users2 } from 'lucide-react';
+import { Volleyball } from 'lucide-react';
+import { Trophy } from 'lucide-react';
 import Footer from '@/components/footer/Footer';
 
 const LandingPage = () => {
   return (
-  <>
-    {/* Hero section */}
-    <div className="bg-background-alt text-primary-foreground relative overflow-hidden" style={{
-      backgroundImage: `url('./src/assets/Dust.png')`,
-      backgroundSize: 'auto',
-      backgroundRepeat: 'repeat'
-      }}>
-      {/* Bg Elements*/}
-      <img 
-        src="./src/assets/hero-element1.png" 
-        alt="Hero Element 1"
-        className="hero-bg-element-base hero-bg-element-bottom-right absolute"
-      />
-      <img 
-        src="./src/assets/hero-element2.png" 
-        alt="Hero Element 2"  
-        className="hero-bg-element-base hero-bg-element-bottom-left absolute"
-      />
-      {/* Main */}
-      <div className="hero-padding-responsive">
-        {/* Logo and subheading */}
-        <div className="flex flex-col items-center gap-[8px]">
-          <img 
-          src="./src/assets/MARS-logo.png" 
-          alt="MARS Logo"
-          className="w-auto h-auto object-contain"/>
-          <h2 className="heading-hero-responsive uppercase text-center">Volleyball League Management System</h2>
-        </div>
-        {/* Buttons */}
-        <div className="flex items-center">
-          <Button variant="secondary" size="lg" className="text-primary uppercase ">
-          Sign In
-          </Button>
-        </div>
-      </div>
-    </div>
-    {/* Features section */}
-    <div className="bg-background-alt-2 relative" style={{
-      backgroundImage: `url('./src/assets/Grunge.png')`,
-      backgroundSize: 'auto',
-      backgroundRepeat: 'repeat'
-      }}>
-      {/* Main*/ }
-      <div className="features-main-responsive">
-        {/* Heading */}
-        <h1 className="features-heading-responsive text-primary-foreground"><span className="text-secondary">League</span> Management Made Easy.</h1>
-        {/* Content */}
-        <div className="features-content-responsive">
-          <div className="features-card-responsive">
-            <Users2 className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Efficient Player Management</h3>
-            <div className="w-full h-[2px] bg-secondary"/>
-            <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">
-              Go beyond basic details with our optimized Player Management feature. This system allows you to effortlessly record, update, and track all necessary player information.
-            </p>
+    <>
+      {/* Hero section */}
+      <div
+        className="bg-background-alt text-primary-foreground relative overflow-hidden"
+        style={{
+          backgroundImage: `url('./src/assets/Dust.png')`,
+          backgroundSize: 'auto',
+          backgroundRepeat: 'repeat',
+        }}
+      >
+        {/* Bg Elements*/}
+        <img
+          src="./src/assets/hero-element1.png"
+          alt="Hero Element 1"
+          className="hero-bg-element-base hero-bg-element-bottom-right absolute"
+        />
+        <img
+          src="./src/assets/hero-element2.png"
+          alt="Hero Element 2"
+          className="hero-bg-element-base hero-bg-element-bottom-left absolute"
+        />
+        {/* Main */}
+        <div className="hero-padding-responsive">
+          {/* Logo and subheading */}
+          <div className="flex flex-col items-center gap-[8px]">
+            <img
+              src="./src/assets/MARS-logo.png"
+              alt="MARS Logo"
+              className="w-auto h-auto object-contain"
+            />
+            <h2 className="heading-hero-responsive uppercase text-center">
+              Volleyball League Management System
+            </h2>
           </div>
-          <div className="features-card-responsive">
-            <Volleyball className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Simplified Team Organization</h3>
-            <div className="w-full h-[2px] bg-secondary rounded" />
-            <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">A comprehensive system that significantly simplifies the organization of all teams by providing a straightforward interface for assigning players, managing team details, and handling administrative updates.</p>
-          </div>
-          <div className="features-card-responsive">
-            <Trophy className="text-secondary w-[40px] h-[40px]"/>
-            <h3 className="text-left text-secondary py-[8px]">Effortless League Creation</h3>
-            <div className="w-full h-[2px] bg-secondary rounded" />
-            <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">Experience a seamless process for creating and managing leagues with our user-friendly tools designed to simplify every step from setup to completion.</p>
+          {/* Buttons */}
+          <div className="flex items-center">
+            <Button variant="secondary" size="lg" className="text-primary uppercase ">
+              Sign In
+            </Button>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
-  </>
+      {/* Features section */}
+      <div
+        className="bg-background-alt-2 relative"
+        style={{
+          backgroundImage: `url('./src/assets/Grunge.png')`,
+          backgroundSize: 'auto',
+          backgroundRepeat: 'repeat',
+        }}
+      >
+        {/* Main*/}
+        <div className="features-main-responsive">
+          {/* Heading */}
+          <h1 className="features-heading-responsive text-primary-foreground">
+            <span className="text-secondary">League</span> Management Made Easy.
+          </h1>
+          {/* Content */}
+          <div className="features-content-responsive">
+            <div className="features-card-responsive">
+              <Users2 className="text-secondary w-[40px] h-[40px]" />
+              <h3 className="text-left text-secondary py-[8px]">Efficient Player Management</h3>
+              <div className="w-full h-[2px] bg-secondary" />
+              <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">
+                Go beyond basic details with our optimized Player Management feature. This system
+                allows you to effortlessly record, update, and track all necessary player
+                information.
+              </p>
+            </div>
+            <div className="features-card-responsive">
+              <Volleyball className="text-secondary w-[40px] h-[40px]" />
+              <h3 className="text-left text-secondary py-[8px]">Simplified Team Organization</h3>
+              <div className="w-full h-[2px] bg-secondary rounded" />
+              <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">
+                A comprehensive system that significantly simplifies the organization of all teams
+                by providing a straightforward interface for assigning players, managing team
+                details, and handling administrative updates.
+              </p>
+            </div>
+            <div className="features-card-responsive">
+              <Trophy className="text-secondary w-[40px] h-[40px]" />
+              <h3 className="text-left text-secondary py-[8px]">Effortless League Creation</h3>
+              <div className="w-full h-[2px] bg-secondary rounded" />
+              <p className="features-paragraph-responsive text-left text-primary-foreground pt-[8px]">
+                Experience a seamless process for creating and managing leagues with our
+                user-friendly tools designed to simplify every step from setup to completion.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
