fdcf8b5ea8edf8432ef0aa886f519602c07659fa
Francis Reid <143309990+Celeru00@users.noreply.github.com>
2025-11-26 21:39:57 +0800

    Revert "chore: moved and refactored img path"
    This reverts commit 56b9844675e6b3f24a15aaa1469382cff7e410d9.

--- team-mars-frontend/src/components/footer/Footer.tsx
+++ team-mars-frontend/src/components/footer/Footer.tsx
@@ -8,9 +8,13 @@ const Footer = () => {
         <div className="border-[2px] border-secondary">
           <div className="footer-padding-responsive">
             <div className="footer-logo-responsive">
-              <img src="/logo.png" alt="MARS Logo" className="w-[60px] h-[60px] object-contain" />
               <img
-                src="/assets/MARS-logo.png"
+                src="./src/assets/logo.png"
+                alt="MARS Logo"
+                className="w-[60px] h-[60px] object-contain"
+              />
+              <img
+                src="./src/assets/MARS-logo.png"
                 alt="MARS Logo"
                 className="w-[200px] h-[60px] object-contain"
               />
