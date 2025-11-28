24b1cb9dc022911b9f9898e07e495bd9315dd933
ejminguez <ebminguez@up.edu.ph>
2025-11-29 07:56:46 +0800

    fix: minor ui bugs

--- team-mars-frontend/src/components/navigation/LoginNavbar.tsx
+++ team-mars-frontend/src/components/navigation/LoginNavbar.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <>
       <nav className="bg-primary w-full p-4 flex flex-col justify-center items-center gap-4">
-        <img src="/assets/logo.png" alt="Logo" className="w-[60px] h-[60px]" />
+        <Link to="/">
+          <img src="/assets/logo.png" alt="Logo" className="w-[60px] h-[60px]" />{' '}
+        </Link>
       </nav>
       <hr className="bg-secondary w-full h-1 border-0" />
     </>
