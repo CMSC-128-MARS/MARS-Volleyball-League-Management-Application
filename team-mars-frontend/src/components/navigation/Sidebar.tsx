3a88ab91657b1e6bb2d82c0cda4d48ad115ef96d
Francis Reid <clruru@Franciss-MacBook-Air.local>
2025-11-28 10:05:20 +0800

    feat: working sidebar

--- team-mars-frontend/src/components/navigation/Sidebar.tsx
+++ team-mars-frontend/src/components/navigation/Sidebar.tsx
@@ -1,20 +1,71 @@
+import { LayoutDashboard, UsersRound, Volleyball, Trophy, Headset } from 'lucide-react';
+
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar"
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
 
-export default function AppSidebar() {
+const items = [
+  {
+    title: 'Dashboard',
+    url: '#',
+    icon: LayoutDashboard,
+  },
+  {
+    title: 'Players',
+    url: '#',
+    icon: UsersRound,
+  },
+  {
+    title: 'Teams',
+    url: '#',
+    icon: Volleyball,
+  },
+  {
+    title: 'Leagues',
+    url: '#',
+    icon: Trophy,
+  },
+  {
+    title: 'Contact Support',
+    url: '#',
+    icon: Headset,
+  },
+];
+
+export function AppSidebar() {
   return (
     <Sidebar collapsible="offcanvas">
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+      <SidebarContent className="bg-primary-alt font-paragraph pt-6 pb-6 pr-1 pl-1">
+        <SidebarGroup>
+          <SidebarGroupLabel className="flex justify-center items-center mb-8">
+            <img src="/assets/logo.png" alt="MARS Logo" className="w-12 h-12" />
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <p className="text-left text-white ml-2 text-[12px]">Navigation</p>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild className="hover:bg-accent rounded-[6px] text-[14px]">
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
-  )
+  );
 }
+
+export default AppSidebar;
