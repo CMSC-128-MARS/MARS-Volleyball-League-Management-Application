25e29bd62d0b7707083143a30846f7fb5ddc57f5
r3j6 <renzjaeprilguiamongaya@gmail.com>
2025-11-28 19:07:26 +0800

    Initialize zustand and Add AuthTest component for Zustand testing, delete the test if the zustand is working

--- team-mars-frontend/src/App.tsx
+++ team-mars-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/navigation/LoginNavbar';
+import AuthTest from './components/AuthTest'; // Import the AuthTest component, for testing only
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
+      <AuthTest />
       <div className="flex-1 overflow-auto">
         <Outlet />
       </div>
