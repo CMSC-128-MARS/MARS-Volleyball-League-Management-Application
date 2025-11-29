7793933a3908b9f48d09621ff3d41f054e681434
Francis Reid <clruru@Franciss-MacBook-Air.local>
2025-11-29 17:56:49 +0800

    chore: fix login styling and responsiveness

--- team-mars-frontend/src/components/forms/contact.tsx
+++ team-mars-frontend/src/components/forms/contact.tsx
@@ -9,13 +9,36 @@ import {
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { Headset } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import { useState, useEffect } from 'react';
 
 export default function Login() {
   const form = useForm();
+  const [rows, setRows] = useState(6);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setRows(2);
+      } else {
+        setRows(6);
+      }
+    };
+
+    // Set initial value
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <Form {...form}>
-      <form className="rounded-sm border border-border relative z-10 bg-white flex flex-col justify-center paragraph-s-regular items-center gap-6 p-8">
+      <form className="w-full rounded-[2px] border border-border relative z-10 bg-white flex flex-col justify-center paragraph-s-regular items-center gap-6 p-8">
         <div className="flex flex-col gap-2 md:w-lg w-full">
           <h4>Contact Support</h4>
           <p className="text-muted-foreground">Input the necessary fields for us to help you</p>
@@ -54,14 +77,15 @@ export default function Login() {
               <FormItem>
                 <FormLabel>Message</FormLabel>
                 <FormControl>
-                  <Textarea placeholder="Enter message" rows={6} {...field} />
+                  <Textarea placeholder="Enter message" rows={rows} {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
         </div>
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button type="submit" className="w-full">
+          <Headset className="w-4 text-white" />
           <p>Done</p>
         </Button>
       </form>
