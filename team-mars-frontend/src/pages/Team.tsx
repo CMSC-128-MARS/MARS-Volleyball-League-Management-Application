5124c1f59b33fcbe75f54c0152b759972a886bd4
Francis Reid <clruru@Franciss-MacBook-Air.local>
2025-12-03 20:47:27 +0800

    feat: card responsiveness and style

--- team-mars-frontend/src/pages/Team.tsx
+++ team-mars-frontend/src/pages/Team.tsx
@@ -4,44 +4,55 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 export default function Team() {
   return (
     <div
-      className="w-full"
+      className="w-full min-h-screen"
       style={{
         backgroundImage: `url('/assets/Grunge.png')`,
         backgroundSize: 'auto',
         backgroundRepeat: 'repeat',
       }}
     >
-      <Tabs defaultValue="view" className="w-full max-w-[400px] mx-auto px-4">
-        <div className="justify-center items-center pt-8 px-8 pb-5 flex flex-col gap-5">
-          <p className="font-heading font-semibold text-2xl text-center ">TEAM MANAGEMENT</p>
-          <TabsList>
-            <TabsTrigger
-              value="view"
-              className="data-[state=active]:bg-primary data-[state=active]:text-white"
-            >
-              View
-            </TabsTrigger>
-            <TabsTrigger
-              value="add"
-              className="data-[state=active]:bg-[#15803D] data-[state=active]:text-white"
-            >
-              Add
-            </TabsTrigger>
-            <TabsTrigger
-              value="remove"
-              className="data-[state=active]:bg-[#D52020] data-[state=active]:text-white"
-            >
-              Remove
-            </TabsTrigger>
-          </TabsList>
-          <hr className="w-full border-t border-muted-foreground" />
-        </div>
-        <TabsContent value="view">
-          <TeamManagementCard />
-        </TabsContent>
-        <TabsContent value="add">Change your password here.</TabsContent>
-        <TabsContent value="remove">Change your password here.</TabsContent>
-      </Tabs>
+      <div className="w-full max-w-[400px] mx-auto px-4">
+        <Tabs defaultValue="view" className="w-full">
+          <div className="justify-center items-center pt-8 px-8 pb-4 flex flex-col gap-5">
+            <p className="font-heading font-semibold text-2xl text-center ">TEAM MANAGEMENT</p>
+
+            <TabsList>
+              <TabsTrigger
+                value="view"
+                className="data-[state=active]:bg-primary data-[state=active]:text-white"
+              >
+                View
+              </TabsTrigger>
+              <TabsTrigger
+                value="add"
+                className="data-[state=active]:bg-[#15803D] data-[state=active]:text-white"
+              >
+                Add
+              </TabsTrigger>
+              <TabsTrigger
+                value="remove"
+                className="data-[state=active]:bg-[#D52020] data-[state=active]:text-white"
+              >
+                Remove
+              </TabsTrigger>
+            </TabsList>
+          </div>
+        </Tabs>
+      </div>
+      <div className="px-4 md:px-10 my-4">
+        <hr className="border-t border-[#A3A3A3]" />
+      </div>
+      <div className="w-full max-w-[400px] mx-auto px-6">
+        <Tabs defaultValue="view" className="w-full">
+          <TabsContent value="view" className="flex flex-col gap-4">
+            <TeamManagementCard />
+            <TeamManagementCard />
+            <TeamManagementCard />
+          </TabsContent>
+          <TabsContent value="add">Add content here.</TabsContent>
+          <TabsContent value="remove">Remove content here.</TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
