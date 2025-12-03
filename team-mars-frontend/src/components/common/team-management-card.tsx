2693ccf8626dbf84dc84c9be7100a4b35c7849ba
Francis Reid <clruru@Franciss-MacBook-Air.local>
2025-12-03 20:47:27 +0800

    feat: card responsiveness and style

--- team-mars-frontend/src/components/common/team-management-card.tsx
+++ team-mars-frontend/src/components/common/team-management-card.tsx
@@ -1,26 +1,43 @@
-import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Volleyball } from 'lucide-react';
 
 export default function TeamManagementCard() {
   return (
-    <Card className="w-full max-w-sm rounded-[2px] border border-border bg-white shadow-lg">
-      <CardHeader>
-        <CardTitle><Volleyball className='text-secondary-alt'/></CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div>
-            Jaepril's Warriors
-        </div>
-      </CardContent>
-    </Card>
+    <div>
+      <Dialog>
+        <DialogTrigger className="w-full max-w-sm rounded-xs border border-border bg-white shadow-lg">
+          <Card>
+            <CardContent className="flex flex-row p-0">
+              <div className="flex justify-center items-center bg-primary w-1/4 p-4 border-r-2 border-secondary">
+                <Volleyball className="text-secondary h-10 w-10" strokeWidth={1} />
+              </div>
+              <div className="flex flex-col justify-start text-left pl-2 py-4 gap-1">
+                <div className="font-heading font-semibold">Jaepril's Warriors</div>
+                <p className="text-xs bg-secondary-alt text-white border rounded-xs px-2 py-1 w-fit">
+                  Players: 6
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>True Vach?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
