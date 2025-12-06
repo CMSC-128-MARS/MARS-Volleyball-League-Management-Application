5c5006523990810cfe9455860d0efa45539bb744
Francis Reid <143309990+Celeru00@users.noreply.github.com>
2025-12-07 01:44:27 +0800

    Feat/remove team (#92)
    * refactor: migrate to axios
    
    * fix: lint issues
    
    * feat: match modules
    
    * refactor: migrate to axios
    
    * fix: lint issues
    
    * feat: remove team dialog
    
    * feat: remove team functionaity
    
    * fix: build and lint issues
    
    ---------
    
    Co-authored-by: Francis Reid <clruru@Franciss-MacBook-Air.local>
    Co-authored-by: Errol Minguez <errolminguez@Errols-MacBook-Air.local>

---
+++ team-mars-frontend/src/pages/TeamDetails.tsx
@@ -0,0 +1,26 @@
+import TeamViewButtons from '@/components/navigation/team-view-buttons';
+import TeamDetailsComponent from '@/components/common/team-details';
+
+export default function TeamDetails() {
+  const handleBack = () => {
+    // TODO: Implement back navigation
+  };
+
+  const handleNext = () => {
+    // TODO: Implement next navigation
+  };
+
+  return (
+    <div
+      className="items-center p-5 md:p-5 lg:p-20 justify-center w-full min-h-[calc(100vh-5rem)]"
+      style={{
+        backgroundImage: `url('/assets/Grunge.png')`,
+        backgroundSize: 'auto',
+        backgroundRepeat: 'repeat',
+      }}
+    >
+      <TeamViewButtons onBack={handleBack} onNext={handleNext} />
+      <TeamDetailsComponent teamName="Team A" leagueName="League 1" teamId="123" />
+    </div>
+  );
+}
